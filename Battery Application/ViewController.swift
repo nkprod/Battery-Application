ac9e2cf8bd4d5f27c3b4b2bb5ff1c1863b3a6308
Nurlybek Karshiga <nurlybekkarshiga@gmail.com>
2020-07-10 08:18:44 -0400

    Initial Commit

---
+++ Battery Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Battery Application
+//
+//  Created by Nulrybek Karshyga on 7/10/20.
+//  Copyright © 2020 Nulrybek Karshyga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
